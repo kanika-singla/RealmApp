cd8276ed60e404dd417bd2dd76f82cbe6a4e8414
kanika-singla <kanika-singla@outlook.com>
2020-10-20 18:06:24 +0200

    removing this

--- functions/deleteMaterial/source.js
+++
@@ -1,7 +0,0 @@
-exports = function(arg){
-    var db = context.values.get("db_name");
-    var coll = context.values.get("coll_name");
-    var collection = context.services.get("mongodb-atlas").db(db).collection(coll);
-    var doc = collection.delete({MatId: arg.MatId});
-  return doc;
-};
--- functions/postMaterial/source.js
+++
@@ -1,7 +0,0 @@
-exports = function(arg){
-    var db = context.values.get("db_name");
-    var coll = context.values.get("coll_name");
-    var collection = context.services.get("mongodb-atlas").db(db).collection(coll);
-    var doc = collection.update({MatId: arg.MatId}, {arg}, {upsert: true});
-    return doc;
-};
